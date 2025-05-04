4678924e7c9221a060d79732229e7ebd0494f2f4
Mohammed <mohammednaamiweb@gmail.com>
2025-05-04 16:06:44 +0200

    implement error showing

--- app/root.tsx
+++ app/root.tsx
@@ -29,14 +29,14 @@ registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning> 
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,7 +1,28 @@
-import { Header } from 'components'
-import React from 'react'
+import { Header, TripCard } from "components";
+import React from "react";
+import { getAllTrips } from "~/appwrite/trips";
+import type { Route } from "./+types/trips";
+import { parseTripData } from "~/lib/utils";
+import type { LoaderFunctionArgs } from "react-router";
 
-const Trips = () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const limit = 8;
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") || "1", 10);
+  const offset = (page - 1) * limit;
+  const { allTrips, total } = await getAllTrips(limit, offset);
+
+  return {
+    trips: allTrips.map(({ $id, tripDetail, imageUrls }) => ({
+      id: $id,
+      ...parseTripData(tripDetail),
+      imageUrls: imageUrls ?? [],
+    })),
+    total,
+  };
+};
+const Trips = ({ loaderData }: Route.ComponentProps) => {
+  const trips = loaderData.trips as Trip[] | [];
   return (
     <main className="dashboard wrapper">
       <Header
@@ -10,8 +31,24 @@ const Trips = () => {
         ctaText="Create New Trip"
         ctaLink="/trips/create"
       />
-      </main>
-  )
-}
+      <section>
+        <h1 className="p-24-semibold text-dark-100">Manage Created Trips</h1>
+        <div className="trip-grid">
+          {trips.map((trip) => (
+            <TripCard
+              key={trip.id}
+              id={trip.id}
+              name={trip.name}
+              imageUrl={trip.imageUrls[0]}
+              location={trip.itinerary?.[0]?.location ?? ""}
+              tags={[trip.interests, trip.travelStyle]}
+              price={trip.estimatedPrice}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
 
-export default Trips
+export default Trips;
